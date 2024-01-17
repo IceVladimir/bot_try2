900b04e80c623297c7013c1044dc50af33b52966
IceVladimir <74771948+IceVladimir@users.noreply.github.com>
2024-01-17 23:36:43 +0300

    asd

--- bot1.js
+++ bot1.js
@@ -4,8 +4,9 @@ const app = express(), port=3001
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 const CharacterAI = require("node_characterai_edited2");
-const characterAI = new CharacterAI();	
-var cd = false;
+//const characterAI = new CharacterAI();	
+const arr_of_puppets = new Map();
+const {v4 : uuidv4} = require('uuid')
 
 app.get('/', (req, res) => {
     res.json(["Tony","Lisa","Michael","Ginger","Food"]);
@@ -13,43 +14,36 @@ app.get('/', (req, res) => {
 
 app.post('/', function(req, res) {
 (async () => {
-if(cd == false){
-  cd = true;
-  await characterAI.authenticateAsGuest();
+  var select_uuid = uuidv4();
+  arr_of_puppets.set(select_uuid, new CharacterAI());
+  await arr_of_puppets.get(select_uuid).authenticateAsGuest();
   //var new_token = await characterAI.getToken1();
   //console.log(new_token);
   //var new_uuid = await characterAI.getUuid1();
   //console.log(new_uuid);
   if(req.body.token != ""){
-  await characterAI.setToken(req.body.token, req.body.uuid)
+  await arr_of_puppets.get(select_uuid).setToken(req.body.token, req.body.uuid)
   }
   // Place your character's id here
   const characterId = "v3lyisRb7INyd5BUdUKEKS1-MUTBom9dY9qV9-2ioTE";
 
-  const chat = await characterAI.createOrContinueChat(characterId);
+  const chat = await arr_of_puppets.get(select_uuid).createOrContinueChat(characterId);
 
   // Send a message
   const response = await chat.sendAndAwaitResponse(req.body.msg, true);
 	
 	
   //console.log(response.text);
-  var new_token = await characterAI.getToken1();
-  var new_uuid = await characterAI.getUuid1();
-  await characterAI.unauthenticate2();
-  cd = false;
+  var new_token = await arr_of_puppets.get(select_uuid).getToken1();
+  var new_uuid = await arr_of_puppets.get(select_uuid).getUuid1();
+  await arr_of_puppets.get(select_uuid).unauthenticate2();
+  arr_of_puppets.delete(select_uuid)
   res.send({
     'Answer': response.text,
 	'Token': new_token,
 	'Uuid': new_uuid,
   });
-}else{
-	console.log("CD!!!");
-	res.send({
-    'Answer': 'CD',
-	'Token': 'CD',
-	'Uuid': 'CD'
-  });
-}	
+
 })();
 });
 app.listen(port, () =>{
